b3a8bb6a0c651411a11efbef0ea969e781fcb862
Antonio Galvão <galvontony@gmail.com>
2021-11-25 11:34:27 -0300

    Add Card styles

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -13,6 +13,8 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    height: 42px;
+
     h2 {
       font-weight: 500;
       font-size: 16px;
